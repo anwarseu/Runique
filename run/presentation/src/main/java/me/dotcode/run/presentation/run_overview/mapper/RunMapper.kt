c5cb1abf3ce214df034dafe0c07fb67fdc5cf552
mhoss32@safeway.com <105231774+anwaralbertsons@users.noreply.github.com>
2024-07-12 15:26:13 -0500

    Setting up the Run models.

---
+++ run/presentation/src/main/java/me/dotcode/run/presentation/run_overview/mapper/RunMapper.kt
@@ -0,0 +1,33 @@
+package me.dotcode.run.presentation.run_overview.mapper
+
+import me.dotcode.core.domain.run.Run
+import me.dotcode.core.presentation.ui.formatted
+import me.dotcode.core.presentation.ui.toFormattedKm
+import me.dotcode.core.presentation.ui.toFormattedKmh
+import me.dotcode.core.presentation.ui.toFormattedMeters
+import me.dotcode.core.presentation.ui.toFormattedPace
+import me.dotcode.run.presentation.run_overview.model.RunUi
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+
+fun Run.toRunUi(): RunUi {
+    val dateTimeInLocalTime = dateTimeUtc
+        .withZoneSameInstant(ZoneId.systemDefault())
+    val formattedDateTime = DateTimeFormatter
+        .ofPattern("MMM dd, yyyy - hh:mma")
+        .format(dateTimeInLocalTime)
+
+    val distanceKm = distanceMeters / 1000.0
+
+    return RunUi(
+        id = id!!,
+        duration = duration.formatted(),
+        dateTime = formattedDateTime,
+        distance = distanceKm.toFormattedKm(),
+        avgSpeed = avgSpeedKmh.toFormattedKmh(),
+        maxSpeed = maxSpeedKmh.toFormattedKmh(),
+        pace = duration.toFormattedPace(distanceKm),
+        totalElevation = totalElevationMeters.toFormattedMeters(),
+        mapPictureUrl = mapPictureUrl
+    )
+}
