5888eccd8b614d5f7c7ae75a37b80799a558140f
mhoss32@safeway.com <105231774+anwaralbertsons@users.noreply.github.com>
2024-07-12 21:14:15 -0500

    Saving pending syncs.

--- core/database/src/main/java/me/dotcode/core/database/RunDatabase.kt
+++ core/database/src/main/java/me/dotcode/core/database/RunDatabase.kt
@@ -3,13 +3,21 @@ package me.dotcode.core.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import me.dotcode.core.database.dao.RunDao
+import me.dotcode.core.database.dao.RunPendingSyncDao
+import me.dotcode.core.database.entity.DeletedRunSyncEntity
 import me.dotcode.core.database.entity.RunEntity
+import me.dotcode.core.database.entity.RunPendingSyncEntity
 
 @Database(
-    entities = [RunEntity::class],
+    entities = [
+        RunEntity::class,
+        RunPendingSyncEntity::class,
+        DeletedRunSyncEntity::class
+    ],
     version = 1
 )
-abstract class RunDatabase: RoomDatabase() {
+abstract class RunDatabase : RoomDatabase() {
 
     abstract val runDao: RunDao
+    abstract val runPendingSyncDao: RunPendingSyncDao
 }
