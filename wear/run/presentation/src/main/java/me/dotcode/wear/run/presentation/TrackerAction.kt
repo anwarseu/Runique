fdfe99f2d7489361777fe81fc2c406211f48c974
mhoss32@safeway.com <105231774+anwaralbertsons@users.noreply.github.com>
2024-07-15 13:25:55 -0500

    Setting up Wear OS permissions handling.

--- wear/run/presentation/src/main/java/me/dotcode/wear/run/presentation/TrackerAction.kt
+++ wear/run/presentation/src/main/java/me/dotcode/wear/run/presentation/TrackerAction.kt
@@ -3,4 +3,5 @@ package me.dotcode.wear.run.presentation
 sealed interface TrackerAction {
     data object OnToggleRunClick: TrackerAction
     data object OnFinishRunClick: TrackerAction
+    data class OnBodySensorPermissionResult(val isGranted: Boolean): TrackerAction
 }
