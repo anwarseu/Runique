91331ba7bff1709e44b0b3cdc4dc146bd071bcb2
mhoss32@safeway.com <105231774+anwaralbertsons@users.noreply.github.com>
2024-07-15 22:39:40 -0500

    Fixed the watch issues.

--- core/connectivity/data/src/main/java/me/dotcode/core/connectivity/data/CoreConnectivityDataModule.kt
+++ core/connectivity/data/src/main/java/me/dotcode/core/connectivity/data/CoreConnectivityDataModule.kt
@@ -1,10 +1,13 @@
 package me.dotcode.core.connectivity.data
 
+import me.dotcode.core.connectivity.data.messaging.WearMessagingClient
 import me.dotcode.core.connectivity.domain.NodeDiscovery
+import me.dotcode.core.connectivity.domain.messaging.MessagingClient
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val coreConnectivityDataModule = module {
+    singleOf(::WearMessagingClient).bind<MessagingClient>()
     singleOf(::WearNodeDiscovery).bind<NodeDiscovery>()
 }
